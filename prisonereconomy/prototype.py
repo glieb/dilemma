b851c0472ce3a2f576e5623be862c9fb9aaa2a45
Gary <gary.lieberman@outlook.com>
2017-06-18 17:39:11 -0400

    shuffled files

--- prisonereconomy/prototype.py
+++ prisonereconomy/prototype.py
@@ -14,7 +14,7 @@ def load_prototype(file):
 
 def create_prototype(**kwargs):
     biases = kwargs
-    remainder = check_biases(biases)  # list of biases not included in kwargs
+    remainder = check_biases(biases)  # list of biases not included
     for bias in remainder:
         biases[bias] = 0  # do not factor unmentioned bases
     return biases
@@ -24,13 +24,15 @@ def check_biases(biases):
     return [bias for bias in bias_dict if bias not in biases]
 
 
+
 if __name__ == "__main__":
-    print("Enter the name of your new prototype, or EXIT to cancel.")
+    print("Enter the name of your new prototype, or EXIT to cancel.") 
     name = input(">>")
     if name == "EXIT":
         exit()
     filename = "prototypes/" + name + ".pkl"
     biases = {}
     for bias in bias_dict:
-        biases[bias] = float(input("Enter {} value for {}:  ".format(bias, name)))
+        biases[bias] = float(input(
+            "Enter {} value for {}:  ".format(bias, name)))
     save_prototype(biases, filename)
